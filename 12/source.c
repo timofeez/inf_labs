b821f56393d9f3ba64ed4a7e6dbf5216ba0eb2e5
Timofey <timofeez@mail.ru>
2024-01-14 00:43:08 +0300

    add lab12

--- 12/source.c
+++ 12/source.c
@@ -1,40 +1,30 @@
 #include <stdio.h>
 
-int length(int n) {
-	if (n < 0) n = -n;
-    if (n == 0) return 1;
-    int l = 0;
-    while(n) {
-        l++;
-        n /= 10;
-        }
-    return l;
-    }
+#define ABS(a) ((a) >= 0 ? (a) : -(a))
 
-int main() {
-	int number;
-	scanf("%d", &number);
-	int sign = number < 0;
-	if (sign) number = -number;
-	int len = length(number);
-	if (len <= 2) {
-		return 1;
-	}	
-	int temp = number;
-	int digits[len];
-	for (int i = len - 1; i >= 0; i--) {
-        digits[i] = temp % 10;
-        temp /= 10;
-    }
-	digits[len - 2] = -1;
-	digits[1] = -1;
-	if (sign) printf("-");	
-	for (int i = 0; i < len; i++) {
-		if (digits[i] != -1) {
-			printf("%d", digits[i]);
-		}
+int power(int a, int b) {
+	int result = a;
+	for (int i = 0; i < b - 1; ++i) {
+		result *= a;
 	}
-	printf("\n");
-	return 0;
+	return result;
+}
+int main(void){
+    int ans;
+    scanf("%d", &ans);
+
+        int ans1 = ABS(ans);
+        int q = 0;
+
+        while (ans1 != 0){
+            q += 1;
+            ans1 /= 10;
+        }
+		if (q == 2){
+            printf("0\n");
+			return 0;
+        }
+		ans = (ans / 100) * 10 + ans % 10;
+		printf("%d\n", (ans / (power(10, q-2)) * power(10, q-3)) + ans % (power(10, q - 3)));
 
 }
